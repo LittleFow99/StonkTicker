a89437886fc905ddf1b8bd0be332a74218366f3c
Thomas Paolino <tpao24@Outlook.com>
2024-09-05 20:31:19 -0400

    Initial Commit

---
+++ StonkTicker/StonkTickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StonkTickerApp.swift
+//  StonkTicker
+//
+//  Created by Thomas Paolino on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct StonkTickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
